3659afb849dda265f6b7ff232f4a17d35ea44465
Paul Nivoix <fozkyz@gmail.com>
2023-09-24 11:59:09 +0200

    Added GameObject class

---
+++ MonoVoxel/Source/Engine/Components/GameObject.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoVoxel
+{
+	public class GameObject
+	{
+		#region Fields
+		public Guid ID { get; private set; }
+		public string Name { get; set; }
+		public string Tag { get; set; }
+		public bool Enabled
+		{
+			get { return Enabled; }
+			set
+			{
+				if (value != _enabled)
+				{
+					_enabled = value;
+					if (_enabled)
+					{
+						OnEnabled();
+					}
+					else
+					{ 
+						OnDisabled(); 
+					}
+				}
+			}
+		}
+
+		public List<GameObject> Children
+		{
+			get { return _children; }
+		}
+
+		#endregion
+
+		private bool _enabled;
+		private List<GameObject> _children;
+
+		public GameObject(string name = "GameObject")
+		{
+			ID = Guid.NewGuid();
+			Name = name;
+			Tag = string.Empty;
+			Enabled = true;
+		}
+
+		public virtual void OnEnabled()
+		{
+			foreach (var child in Children)
+			{
+				child.Enabled = true;
+			}
+		}
+
+		public virtual void OnDisabled()
+		{
+			foreach (var child in Children)
+			{
+				child.Enabled = false;
+			}
+		}
+	}
+}
---
+++ MonoVoxel/Source/Game/Main.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoVoxel
+{
+    public class Main : Game
+	{
+        private GraphicsDeviceManager _graphics;
+
+        public Main()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+    }
+
+#if WINDOWS || LINUX
+    public static class Program
+    {
+        static void Main()
+        {
+            using (var game = new Main())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
